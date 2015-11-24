0f316b3015ac0c600535ce57c2edd1ae51dd2d0e
Euan Thoms <euan@potensol.com>
2015-11-24 13:09:12 +0800

    Regenerated patches for net/opensips using 'make makepatch'.

--- net/opensips/files/patch-modules__proto_tls__proto_tls.c
+++ net/opensips/files/patch-modules__proto_tls__proto_tls.c
@@ -1,5 +1,5 @@
---- modules/proto_tls/proto_tls.c.orig	2015-08-27 23:06:47.000000000 +0800
-+++ modules/proto_tls/proto_tls.c	2015-11-24 04:59:44.764931027 +0800
+--- modules/proto_tls/proto_tls.c.orig	2015-08-27 15:06:47 UTC
++++ modules/proto_tls/proto_tls.c
 @@ -42,6 +42,7 @@
  #include <openssl/opensslv.h>
  #include <openssl/err.h>
